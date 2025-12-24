802179f033f1a8f0f2789649fe18517085f4d19f
acoulibaly-ghb <abdoulaye.coulibaly@gmail.com>
2025-12-24 01:11:46 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,7 @@ export default defineConfig({
   plugins: [react()],
   define: {
     // Injecte les variables d'environnement système (Vercel) ou locales
-    // Injection d'une variable factice ou suppression pour sécurité
-    'process.env.API_KEY': JSON.stringify('proxied'),
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || process.env.VITE_API_KEY || ''),
     'process.env.TEACHER_PASSWORD': JSON.stringify(process.env.TEACHER_PASSWORD || process.env.VITE_TEACHER_PASSWORD || 'admin'),
   },
 });
